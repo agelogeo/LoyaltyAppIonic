4ba2b022a6914e05fb8b1e6b10177c0bb9d14e3d
Agelogeo <agelo1995@gmail.com>
2017-10-15 21:24:15 +0300

    side menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {CustomerHomePage} from "../pages/customer-home/customer-home";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = HomePage;
+
+  customerPages : Array<{title:string,component:any,icon:string}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +22,15 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.customerPages = [
+      {title: 'Home' , component: CustomerHomePage, icon: 'home'}
+    ];
   }
+
+  openPage(page){
+    this.nav.setRoot(page.component);
+  }
+
 }
 
